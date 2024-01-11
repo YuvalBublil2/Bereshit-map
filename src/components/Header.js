a24241c43910c403b187bc691b3e0171a4e5444b
Yuval Bublil <yuvalbublil12345@gmail.com>
2024-01-11 23:00:24 +0200

    margin fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import MatbishIcon from './MatbishIcon';
 
 function Header() {
     return (
-        <header className="pb-3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '1%' }}>
+        <header className="pb-3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '10%' }}>
             <div style={{ position: 'absolute', left: 0 }}>
                 <MatbishIcon />
             </div>
