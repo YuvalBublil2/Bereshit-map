9ba5d18f97dace80b0b5202d4ef239e14b7b8813
Yuval Bublil <yuvalbublil12345@gmail.com>
2024-01-13 12:08:53 +0200

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header id={"header"} className="pb-3">
             <div className="header-content">
-                <h2>Bereshit Map</h2>
+                <h1>Bereshit Map</h1>
             </div>
         </header>
     );
