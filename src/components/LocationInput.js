813d1bea753a403e09c526221e563979231fb62d
Yuval Bublil <yuvalbublil12345@gmail.com>
2024-02-01 21:16:07 +0200

    Delete "Enter Location" tex in the input area

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -2,7 +2,6 @@
 const LocationInput = ({ onSearch }) => {
   return (
     <div className={"input-group mb-3 shadow-sm"}>
-      <span className={"input-group-text"}>Enter Location:</span>
       <input id={"name-location"} type={"text"} placeholder={"location name:"} className={"form-control"}/>
       <button className={"btn btn-primary"} onClick={onSearch}>Search</button>
     </div>
